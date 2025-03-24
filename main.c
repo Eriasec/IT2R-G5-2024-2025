66570bc193d170193275146237192c0d7fd18f37
Randimbi <andriantsitohaina.randimbimananjara@etu-upsaclay.fr>
2025-03-24 11:10:48 +0100

    V2
    Moteur avant, arrière

--- main.c
+++ main.c
@@ -1,113 +1,93 @@
 #include "LPC17xx.h"                    // Device header
 #include "GPIO_LPC17xx.h"               // Keil::Device:GPIO
 
-
-
-
 // Définition des broches
 #define IN_A       (1 << 16)  // GPIO P0.16
-#define IN_B       (1 << 11)  // GPIO P0.11
+#define IN_B       (1 << 17)  // GPIO P0.17
 #define EN_A       (1 << 19)  // GPIO P0.19 
 #define EN_B       (1 << 18)  // GPIO P0.18 
-#define PWM_PIN  (1 << 25)  // GPIO P3.25 (PWM Moteur)
-#define SERVO_PIN  (1 << 26)  // GPIO P3.26 (PWM Servo) - Direction
-
+#define PWM_PIN    (1 << 25)  // GPIO P3.25 (PWM Moteur)
 
 
 
 // Configuration du PWM
-#define PWM_FREQUENCY_Moteur     20000  // 20 kHz pour le moteur
-#define PWM_FREQUENCY_Servo     		50  // 50 Hz pour le moteur
-#define SERVO_MIN_PULSE   1000  				// 1000 µs pour 0°
-#define SERVO_MAX_PULSE   2000 				 // 2000 µs pour 180°
-
-
+#define PWM_FREQUENCY_Moteur     10000  	// 10 kHz pour le moteur
 
 // Prototypes des fonctions
 void InitGPIO(void);
-void InitPWM(void);
-void SetServoAngle(unsigned int angle);
+void InitPWM(int);
 void Avancer(char vitesse);
 void Reculer(char vitesse);
 void DelayMs(unsigned int ms);
 
 // Initialisation des GPIO
-void InitGPIO(void) {
+void InitGPIO(void) 
+{
     LPC_GPIO0->FIODIR |= (IN_A | IN_B | EN_A | EN_B);  // Configurer comme sorties
-    LPC_GPIO3->FIODIR |= PWM_PIN;  // Configurer P3.25 comme sortie pour PWM
-		LPC_GPIO3->FIODIR |= SERVO_PIN; // Configurer P3.26 comme sortie
+    
+//		LPC_GPIO3->FIODIR |= SERVO_PIN; // Configurer P3.26 comme sortie
 }
 
 // Initialisation du PWM -- Moteur
-void InitPWM(void) {
-    LPC_SC->PCONP |= (1 << 6);  															// Activer PWM1
-    LPC_PINCON->PINSEL3 |= (2 << 18) | (2 << 20); 						// Configurer P3.25 en PWM1.2 et P3.26 en PWM1.1
-    
-    LPC_PWM1->PR = 0; 																				// Pas de prescaler
-    LPC_PWM1->MR0 = SystemCoreClock / PWM_FREQUENCY_Moteur; 	// Période du PWM - Match MR du PWM
-    LPC_PWM1->MCR = (1 << 1); 																// Reset sur MR0 - RAZ du compteur
-    LPC_PWM1->PCR = (1 << 9) | (1 << 10); 										// Activer PWM1.1 et PWM1.2
-    LPC_PWM1->TCR = (1 << 0) | (1 << 3); 											// Activer le compteur (1<<0) et PWM
-}
+void InitPWM(int alpha)
+{
+		// Utilisation d la PWM1.2 
+		// MR2 rapport cyclique alpha du moteur
+		// ils varient de 0 à 99 (cas MR0 associé à 99)
+		// La fréquence de la PWM vaut 10KHz soit 100 us
+		// la fréquence de base est donnée par l'horloge interne de fréquence 25 MHz soir 40ns
 
-// Définir l'angle du servo de 0° à 180°
-void SetServoAngle(unsigned int angle) {
-    unsigned int pulse_width;
-    unsigned int match_value;
-    
-    if (angle > 180) angle = 180;
-    pulse_width = SERVO_MIN_PULSE + (angle * (SERVO_MAX_PULSE - SERVO_MIN_PULSE) / 180);
-    match_value = (pulse_width * SystemCoreClock) / (PWM_FREQUENCY_Servo * 1000000);
-    
-    LPC_PWM1->MR1 = match_value;
-    LPC_PWM1->LER = (1 << 1);
+		LPC_SC->PCONP = LPC_SC->PCONP | 0x00000040;   // enable PWM1
+
+		// calcul des diviseurs
+		// prescaler+1 = 1us/40ns = 25 cela donne une horloge de base de fréquence 10 KHz = 100 us
+		// valeur + 1 = 100 cela fait 1ms 
+		// la duree de comptage vaut 1 us
+
+		LPC_PWM1->PR = 24;  // prescaler
+		LPC_PWM1->MR0 = 99;    // MR0+1=100   la période de la PWM vaut 100µs
+
+		LPC_PINCON->PINSEL7 = LPC_PINCON->PINSEL7| (3 << 18); // P3.25 est la sortie 1 PWM1   bit19 & bit18 
+
+																	
+		LPC_PWM1->MCR = LPC_PWM1->MCR | 0x00000002; // Compteur relancé quand MR0 repasse à 0
+		LPC_PWM1->LER = LPC_PWM1->LER | 0x0000000F;  // ceci donne le droit de modifier dynamiquement la valeur du rapport cyclique
+		// bit 0 = MR0    bit 1 MR1 bit2 MR2 bit3 = MR3
+		LPC_PWM1->PCR = LPC_PWM1->PCR | 0x00000e00;  // autorise les sortie PWM1/2/3 bits 9, 10, 11
+
+
+		LPC_PWM1->MR2 = alpha;							//Rapport cyclique alpha OU vitesse
+
+		LPC_PWM1->TCR = 1;  /*validation de timer  et reset counter */
 }
 
 // Avancer avec une vitesse donnée
-void Avancer(char vitesse) {
-    LPC_GPIO0->FIOSET = IN_A;
-    LPC_GPIO0->FIOCLR = IN_B;
-    LPC_GPIO0->FIOSET = EN_A | EN_B;
-    LPC_PWM1->MR2 = (vitesse * LPC_PWM1->MR0) / 100;
-    LPC_PWM1->LER = (1 << 2);
+void Avancer(char vitesse) 
+{
+    LPC_GPIO0-> FIOPIN |= IN_A;					// IN_A à 1
+    LPC_GPIO0-> FIOPIN &=~ IN_B;				// IN_B à 0
+    LPC_GPIO0-> FIOPIN |= EN_A | EN_B;	// EN_A & EN_B à 1
+    LPC_PWM1->MR2 = vitesse;						// Rapport cyclique alpha OU vitesse
 }
 
 // Reculer avec une vitesse donnée
-void Reculer(char vitesse) {
-    LPC_GPIO0->FIOCLR = IN_A;
-    LPC_GPIO0->FIOSET = IN_B;
-    LPC_GPIO0->FIOSET = EN_A | EN_B;
-    LPC_PWM1->MR2 = (vitesse * LPC_PWM1->MR0) / 100;
-    LPC_PWM1->LER = (1 << 2);
-}
-
-// Délai basé sur Timer0
-void DelayMs(unsigned int ms) {
-    LPC_TIM0->TCR = 0x02;  // Reset timer
-    LPC_TIM0->PR = 0;      // Pas de prescaler
-    LPC_TIM0->MR0 = (SystemCoreClock / 1000) * ms;
-    LPC_TIM0->MCR = 0x04;  // Arrêt sur correspondance
-    LPC_TIM0->TCR = 0x01;  // Démarrer le timer
-    while (LPC_TIM0->TCR & 0x01);
+void Reculer(char vitesse) 
+{
+    LPC_GPIO0-> FIOPIN &=~ IN_A; 				// IN_A à 0 P0.16
+    LPC_GPIO0-> FIOPIN |= IN_B;					// IN_B à 1 P0.17
+    LPC_GPIO0-> FIOPIN |= EN_A | EN_B;		// EN_A & EN_B à 1
+    LPC_PWM1->MR2 = vitesse;						// Rapport cyclique alpha OU vitesse
 }
 
 // Fonction principale
 int main(void) {
-    char vitesse = 50;
+    char alpha = 40;
     InitGPIO();
-    InitPWM();
+    InitPWM(alpha);
 
     while (1) {
-        Avancer(vitesse);
-        SetServoAngle(0);
-        DelayMs(1000);
-
-        Reculer(vitesse);
-        SetServoAngle(90);
-        DelayMs(1000);
-
-        Avancer(vitesse);
-        SetServoAngle(180);
-        DelayMs(1000);
+//        Avancer(50);
+				Reculer(40);
     }
 }
+
