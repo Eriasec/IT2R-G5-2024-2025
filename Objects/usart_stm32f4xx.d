346df9140b630ae9c61a071f068cff30cbe50a25
LICA <liam.cantais@gmail.com>
2025-03-28 12:14:52 +0100

    V.2
    Optimisation du programme + ajout callback UART

--- Objects/usart_stm32f4xx.d
+++ Objects/usart_stm32f4xx.d
@@ -7,8 +7,8 @@
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_Common.h
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h
-.\objects\usart_stm32f4xx.o: U:\PROJET_IT2R\GPS\GPS\RTE\Device\STM32F407VGTx\stm32f4xx_hal_conf.h
-.\objects\usart_stm32f4xx.o: U:\PROJET_IT2R\GPS\GPS\RTE\RTE_Components.h
+.\objects\usart_stm32f4xx.o: C:\Users\TEMP.GEII2.017\Documents\GitHub\IT2R-G5-2024-2025\RTE\Device\STM32F407VGTx\stm32f4xx_hal_conf.h
+.\objects\usart_stm32f4xx.o: C:\Users\TEMP.GEII2.017\Documents\GitHub\IT2R-G5-2024-2025\RTE\RTE_Components.h
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_rcc.h
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_def.h
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h
@@ -32,4 +32,4 @@
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr.h
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h
 .\objects\usart_stm32f4xx.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_usart.h
-.\objects\usart_stm32f4xx.o: U:\PROJET_IT2R\GPS\GPS\RTE\Device\STM32F407VGTx\RTE_Device.h
+.\objects\usart_stm32f4xx.o: C:\Users\TEMP.GEII2.017\Documents\GitHub\IT2R-G5-2024-2025\RTE\Device\STM32F407VGTx\RTE_Device.h
--- main.c
+++ main.c
@@ -2,88 +2,109 @@
 #include "Driver_CAN.h"                 // ::CMSIS Driver:CAN
 #include <stdio.h>
 #include <string.h>
-extern ARM_DRIVER_USART Driver_USART3;
-extern ARM_DRIVER_USART Driver_USART2;
-extern ARM_DRIVER_CAN Driver_CAN1;
+extern ARM_DRIVER_USART Driver_USART3; // USART3 pour réception d'une trame - Broche PD8/9
+extern ARM_DRIVER_USART Driver_USART2; // USART2 pour renvoi trame traitée pour test - Broche PA2/3
 
-//void Init_CAN(void){
-//  Driver_CAN1.Initialize(NULL, NULL);
-//  Driver_CAN1.PowerControl(ARM_POWER_FULL);
-//  Driver_CAN1.SetMode(ARM_CAN_MODE_INITIALIZATION);
-//  Driver_CAN1.SetBitrate( ARM_CAN_BITRATE_NOMINAL,
-//                          125000,
-//                          ARM_CAN_BIT_PROP_SEG(5U) |
-//                          ARM_CAN_BIT_PHASE_SEG1(1U) |
-//                          ARM_CAN_BIT_PHASE_SEG2(1U) |
-//                          ARM_CAN_BIT_SJW(1U)
-//);
-//}
+#define GPS_BUF_TAILLE 300  // Pour faciliter les changements 
 
-void Init_UART(void){
-	Driver_USART3.Initialize(NULL);        //Recevoir une trame - Broche PD8/9
-	Driver_USART3.PowerControl(ARM_POWER_FULL);
-	Driver_USART3.Control(	ARM_USART_MODE_ASYNCHRONOUS |
-							ARM_USART_DATA_BITS_8		|
-							ARM_USART_STOP_BITS_1		|
-							ARM_USART_PARITY_NONE		|
-							ARM_USART_FLOW_CONTROL_NONE,
-							9600);
-	Driver_USART3.Control(ARM_USART_CONTROL_TX,1);
-	Driver_USART3.Control(ARM_USART_CONTROL_RX,1);
+
+char gps_buffer[GPS_BUF_TAILLE];       // Trame reçue
+
+float time_utc;           // UTC TIME : hhmmss.sss
+char status;              // "A" si DATA est valide ou "V" si invalide
+float latitude;           // Latitude : ddmm.mmmm
+char NS;                  // NORD ou SUD : "N" ou "S"
+float longitude;          // Longitude : dddmm.mmmm
+char EW;                  // EST ou OUEST : "E" ou "W"
+
+
+volatile int gps_data_ready = 0;  // Flag mis à 1 lorsque la trame est complètement reçue / volatile pour le compilateur
+
+
+void GPS_UART_Callback(uint32_t event) {     // fonction Callback appelée lorsque la trame est reçue et exploitable
+    if (event & ARM_USART_EVENT_RECEIVE_COMPLETE) {
+        gps_data_ready = 1;                  // Signale que la trame est prête à être traitée
+    }
+}
   
-  Driver_USART2.Initialize(NULL);                   //Envoyer la trame traitée pour tester - Broche PA2/3  
-	Driver_USART2.PowerControl(ARM_POWER_FULL);
-	Driver_USART2.Control(	ARM_USART_MODE_ASYNCHRONOUS |
-							ARM_USART_DATA_BITS_8		|
-							ARM_USART_STOP_BITS_1		|
-							ARM_USART_PARITY_NONE		|
-							ARM_USART_FLOW_CONTROL_NONE,
-							9600);
-	Driver_USART2.Control(ARM_USART_CONTROL_TX,1);
-	Driver_USART2.Control(ARM_USART_CONTROL_RX,1);
+void Init_UART_GPS(void) {
+    Driver_USART3.Initialize(GPS_UART_Callback);    // Initialisation de l’UART3 avec la fonction callback
+    Driver_USART3.PowerControl(ARM_POWER_FULL);
+    Driver_USART3.Control(ARM_USART_MODE_ASYNCHRONOUS |
+                          ARM_USART_DATA_BITS_8 |
+                          ARM_USART_STOP_BITS_1 |
+                          ARM_USART_PARITY_NONE |
+                          ARM_USART_FLOW_CONTROL_NONE,
+                          9600);
+    Driver_USART3.Control(ARM_USART_CONTROL_TX, 1);
+    Driver_USART3.Control(ARM_USART_CONTROL_RX, 1);
+
+    Driver_USART3.Receive(gps_buffer, GPS_BUF_TAILLE);  // On démarre la réception de la trame
 }
 
-int main (void){
 
-	char trame[500];       // Trame recue
-  
-  char id[] = "$GPRMC";  // ID du message compris dans la trame
-  float  time;           // UTC TIME : hhmmss.sss
-  char status;           // "A" si DATA est valide ou "V" si invalide
-  float latitude;        // Latitude : ddmm.mmmm
-  char NS;               // NORD ou SUD : "N" ou "S"
-  float longitude;
-  char EW;               // EST ou OUEST : "E" ou "W"
-  char debugBuf[100];
-  int valide;       // Nombre de champs correctement scannés et affectés
+void Init_UART_Debug(void) {              // Initialisation de l’UART2 pour debug
+    Driver_USART2.Initialize(NULL);    
+    Driver_USART2.PowerControl(ARM_POWER_FULL);
+    Driver_USART2.Control(ARM_USART_MODE_ASYNCHRONOUS |
+                          ARM_USART_DATA_BITS_8 |
+                          ARM_USART_STOP_BITS_1 |
+                          ARM_USART_PARITY_NONE |
+                          ARM_USART_FLOW_CONTROL_NONE,
+                          9600);
+    Driver_USART2.Control(ARM_USART_CONTROL_TX, 1);
+    Driver_USART2.Control(ARM_USART_CONTROL_RX, 1);
+}
 
-  char *p;     //Pointe au début de la séquence "$GPRMC..."
-  char msg[] = "Pas de $GPRMC dans ce bloc"; // Message d'erreur
-  
-  Init_UART();
-  
 
-	while (1)
-  {
-		Driver_USART3.Receive(trame, 500);  // On reçoit la trame du module GPS
-    while (Driver_USART3.GetRxCount() < 80);
+void GPS_TraiterTrame(void) {   // Fonction de traitement d’une trame GPRMC reçue
+    char* debut_trame;      
+    char* fin_trame;  
+    char debugBuf[200];      
+    int champs_valides;      
 
-    p = strstr(trame, "$GPRMC"); // On cherche "$GPRMC", si on trouve, on a l'adresse de la case correspondante
+    debut_trame = strstr(gps_buffer, "$GPRMC");   // Recherche de la trame GPRMC dans le buffer, si on la trouve, on pointe à l'adresse de la case correspondante
+    if (debut_trame != NULL) {
+       
+        fin_trame = strstr(gps_buffer, "CRLF"); 
+        if (fin_trame != NULL){
+            sprintf(debugBuf, "pas de fin de trame $GPRMC\r\n");
+        }
+        champs_valides = sscanf(debut_trame, "$GPRMC,%f,%c,%f,%c,%f,%c",   // On scanne les données
+                                &time_utc, &status,
+                                &latitude, &NS,
+                                &longitude, &EW);
 
-    if (p != NULL) 
-    {
-      valide = sscanf(p, "$GPRMC,%f,%c,%f,%c,%f,%c",&time, &status, &latitude, &NS, &longitude, &EW);
-
-      sprintf(debugBuf, "valide=%d, lat=%.4f, %c, lon=%.4f, %c, status=%c\r\n", valide, latitude, NS, longitude, EW, status); // On scanne les données
-      while (Driver_USART2.GetStatus().tx_busy);
-      Driver_USART2.Send(debugBuf, strlen(debugBuf));
+        if (champs_valides == 6) {
+            sprintf(debugBuf, "valide=%d,lat=%.4f,%c,lon=%.4f,%c", champs_valides, latitude, NS, longitude, EW); // Préparation de l'affichage des données valides pour debug
+        } else {
+            sprintf(debugBuf, "Trame invalide !\r\n");
+        }
+    } else {
+        
+        sprintf(debugBuf, "Pas de $GPRMC trouvé.\r\n");   // Envoi message d'erreur si trame pas trouvée
     }
 
-    else // Envoi message d'erreur
-    {
-      while (Driver_USART2.GetStatus().tx_busy);
-      Driver_USART2.Send(msg, strlen(msg));
+    
+    while (Driver_USART2.GetStatus().tx_busy);
+    Driver_USART2.Send(debugBuf, strlen(debugBuf));
+
+    
+    gps_data_ready = 0;        // On réinitialise pour relancer le cycle
+    Driver_USART3.Receive(gps_buffer, GPS_BUF_TAILLE);
+}
+
+
+int main(void) {
+    Init_UART_GPS();     // Initialisation UART avec le module GPS
+    Init_UART_Debug();   // Initialisation UART pour debug
+
+    while (1) {
+        
+        if (gps_data_ready) {   // Si la trame a été reçue complètement
+            GPS_TraiterTrame();  // On traite les données
+        }
+
+        
     }
-	}	
-	return 0;
 }
